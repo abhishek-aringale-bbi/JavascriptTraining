1bd78ee74d6d05ae4fcb3ce140f97e0a05c9589e
abhishek-aringale-bbi <abhishek.aringale@media4u.com>
2023-09-06 12:31:56 +0530

    challege 4

--- Exercise8.js
+++ Exercise8.js
@@ -11,3 +11,14 @@ for (let i = 0; i < bills.length; i++) {
     tips.push(calcTip(bills[i]));
     totals.push(tips[i] + bills[i]);
 }
+console.log(tips, totals);
+
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+
+console.log(calcAverage(totals));
